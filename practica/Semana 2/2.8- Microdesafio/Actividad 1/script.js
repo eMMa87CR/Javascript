7f89615efdac01bd7593e7c3c1020a15db16cef9
Victor Emmanuel Mercado <vem696@gmail.com>
2024-06-24 00:02:04 -0300

    microdesafio act.1

---
+++ practica/Semana 2/2.8- Microdesafio/Actividad 1/script.js
@@ -0,0 +1,5 @@
+let cantidad = parseInt(prompt('INGRESE CANTIDAD DE REPETICIONES'), 10);
+let texto = prompt('INGRESE TEXTO A REPETIR');
+for (let index = 0; index < cantidad; index++) {
+    console.log(texto);
+}
